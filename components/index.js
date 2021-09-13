5336920b02d52bce9196c95dc2906be17adea6a3
ebenezerdon <ebenezerdonu@gmail.com>
2021-09-13 20:18:16 +0100

    export Nav component from index.js

--- components/index.js
+++ components/index.js
@@ -1,7 +1,4 @@
-import Header from './Header'
+import Nav from './Nav'
 import Post from './Post'
 
-const components = { Header, Post }
-
-export default components
-export { Header, Post }
+export { Nav, Post }
