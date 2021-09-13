78eb27c3f9f66d197b453323a016c154a493e4f0
ebenezerdon <ebenezerdonu@gmail.com>
2021-09-13 20:16:58 +0100

    add bootstrap CDN to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,23 @@
 import { MDXProvider } from '@mdx-js/react'
 import '../styles/globals.css'
-import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <MDXProvider>
-      <Header />
-      <main>
-        <Component {...pageProps} />
-      </main>
+      <Head>
+        <title>Next and MDX Blog</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+              crossOrigin="anonymous" />
+      </Head>
+      <div className="container">
+        <Nav />
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
     </MDXProvider>
   )
 }
